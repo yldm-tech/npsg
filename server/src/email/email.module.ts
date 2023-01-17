1a966be685736aaabd6c7718e2b1cb6e2b47b520
Evan <suzukaze.hazuki2020@gmail.com>
2023-01-17 15:50:25 +0900

    move the server

---
+++ server/src/email/email.module.ts
@@ -0,0 +1,5 @@
+// import { Module } from '@nestjs/common';
+// import { EmailService } from './email.service';
+//
+// @Module({ imports: [], controllers: [], providers: [EmailService] })
+// export class EmailModule {}
---
+++ server/src/email/email.service.ts
@@ -0,0 +1,11 @@
+// import { MailerService } from '@nestjs-modules/mailer';
+// import { Injectable } from '@nestjs/common';
+//
+// @Injectable()
+// export class EmailService {
+//   constructor(private mailService: MailerService) {}
+//
+//   sendMail(to: string) {
+//     this.mailService.sendMail({ to });
+//   }
+// }
