5060a51a3f0266f94e63e714882f94305d43aed8
xiaomo <suzukaze.hazuki2020@gmail.com>
2023-01-24 21:34:48 +0900

    response

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -2,10 +2,12 @@ import {
   Controller,
   FileTypeValidator,
   Get,
+  Header,
   MaxFileSizeValidator,
   ParseFilePipe,
   Post,
   Res,
+  StreamableFile,
   UploadedFile,
   UploadedFiles,
   UseInterceptors,
@@ -13,6 +15,9 @@ import {
 import { FileService } from './file.service';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 import { FileSizeValidationPipe } from 'src/common/pipe/file-size.pipe';
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { Response } from 'express';
 
 @Controller('file')
 export class FileController {
@@ -53,10 +58,25 @@ export class FileController {
   }
 
   @Get('download')
+  @Header('Content-Type', 'application/octet-stream')
+  @Header('Content-Disposition', 'attachment; filename="download.tar"')
   async downloadFile(@Res() res: Response) {
     const { filename, stream } = await this.fileService.downloadFile();
-    res.headers.set('Content-Type', 'application/octet-stream');
-    res.headers.set('Content-Disposition', `attachment; filename=${filename}`);
     return stream.pipe(res);
+  }
+  @Get()
+  @Header('Content-Type', 'application/json')
+  @Header('Content-Disposition', 'attachment; filename="package.json"')
+  getFile(@Res({ passthrough: true }) res: Response): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'package.json'));
+    return new StreamableFile(file);
+  }
+
+  @Get('static')
+  @Header('Content-Type', 'application/json')
+  @Header('Content-Disposition', 'attachment; filename="package.json"')
+  getStaticFile(): StreamableFile {
+    const file = createReadStream(join(process.cwd(), 'package.json'));
+    return new StreamableFile(file);
   }
 }
