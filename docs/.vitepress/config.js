c2c647361054284288e9a29f8ed290bd49c23a10
Evan <suzukaze.hazuki2020@gmail.com>
2023-01-25 22:10:14 +0900

    fix favico

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,6 +6,53 @@ import sidebar from './configs/sidebar';
 export default defineConfig({
   title: 'Npsg',
   description: '基于Nestjs的server框架',
+  head: [
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/assets/favicons/apple-touch-icon.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/assets/favicons/favicon-32x32.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/assets/favicons/favicon-16x16.png',
+      },
+    ],
+    ['link', { rel: 'manifest', href: '/assets/favicons/site.webmanifest' }],
+    [
+      'link',
+      {
+        rel: 'mask-icon',
+        href: '/assets/favicons/safari-pinned-tab.svg',
+        color: '#3a0839',
+      },
+    ],
+    ['link', { rel: 'shortcut icon', href: '/assets/favicons/favicon.ico' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#3a0839' }],
+    [
+      'meta',
+      {
+        name: 'msapplication-config',
+        content: '/assets/favicons/browserconfig.xml',
+      },
+    ],
+    ['meta', { name: 'theme-color', content: '#ffffff' }],
+  ],
 
   lastUpdated: true,
   cleanUrls: 'without-subfolders',
