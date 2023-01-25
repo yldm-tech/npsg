2370302de2d6b16847e93d1c3b74ca93142c954b
Evan <suzukaze.hazuki2020@gmail.com>
2023-01-25 21:55:05 +0900

    base docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,68 @@
+import { defineConfig } from 'vitepress';
+import { demoBlockPlugin } from 'vitepress-theme-demoblock';
+import nav from './configs/nav';
+import sidebar from './configs/sidebar';
+
+export default defineConfig({
+  title: 'Npst',
+  description: '基于Nestjs的server框架',
+
+  lastUpdated: true,
+  cleanUrls: 'without-subfolders',
+
+  base: process.env.BASE || '/',
+  head: [['link', { rel: 'icon', type: 'image/svg+xml', href: '/logo.png' }]],
+
+  markdown: {
+    headers: {
+      level: [0, 0],
+    },
+
+    // options for markdown-it-anchor
+    anchor: { permalink: false },
+
+    // options for markdown-it-toc
+    toc: { includeLevel: [1, 2] },
+
+    // light: #f9fafb, dark: --vp-code-block-bg
+    theme: { light: 'github-light', dark: 'github-dark' },
+
+    config: (md) => {
+      md.use(demoBlockPlugin, {
+        cssPreprocessor: 'less',
+      });
+    },
+  },
+
+  themeConfig: {
+    outlineTitle: '本页目录',
+    lastUpdatedText: '上次更新',
+    logo: '/logo.png',
+
+    algolia: {
+      appId: 'TEEBDL6LJX',
+      apiKey: '6c09da47a1ddf9596d3c6b4a156c2e69',
+      indexName: 'npsg-index',
+    },
+
+    // nav
+    nav,
+
+    // sidebar
+    sidebar,
+
+    editLink: {
+      pattern: 'https://github.com/lantron-ltd/npsg/edit/main/docs/:path',
+      text: '在 GitHub 上编辑此页',
+    },
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/lantron-ltd/npsg' },
+    ],
+
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023 npst',
+    },
+  },
+});
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,35 @@
-# Hello VitePress
+---
+layout: home
+
+title: npst
+titleTemplate: 使用nestjs、prisma、swagger搭建的开箱即用的脚手架。
+
+hero:
+  name: Npst
+  text: 基于Nest搭建的Server Framework
+  # tagline: 基于NestJS搭建的后端框架
+  image:
+    src: /logo.png
+    alt: npsg
+  actions:
+    - theme: brand
+      text: 开始
+      link: /guide/
+    - theme: alt
+      text: 更多
+      link: https://github.com/lantron-ltd/npsg
+
+features:
+  - icon: ⚡️
+    title: 开箱即用
+    details: Feel the speed of Vite. Instant server start and lightning fast HMR that stays fast regardless of the app size.
+  - icon: 💡
+    title: 功能完备
+    details: With Markdown-centered content, it's built to help you focus on writing and deployed with minimum configuration.
+  - icon: 🛠️
+    title: 开发快速
+    details: Enhance your content with all the features of Vue in Markdown, while being able to customize your site with Vue.
+  - icon: 🔑
+    title: 
+    details: Go wild with true SSG + SPA architecture. Static on page load, but engage users with 100% interactivity from there.
+---
