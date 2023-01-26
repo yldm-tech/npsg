2052696799aa85e4756dc7666b06d6e7085c1a9b
Evan <suzukaze.hazuki2020@gmail.com>
2023-01-26 18:22:38 +0900

    fix(fix docs compile error): readd the style and vue file

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,15 +1,14 @@
-import DefaultTheme from 'vitepress/theme'
-import 'vitepress-theme-demoblock/dist/theme/styles/index.css'
-import { useComponents } from './useComponents'
-import './styles/index.css'
-import Button from '../../../src/components/Button.vue'
-import '../../../src/styles/index.css'
+import DefaultTheme from 'vitepress/theme';
+import 'vitepress-theme-demoblock/dist/theme/styles/index.css';
+import { useComponents } from './useComponents';
+import './styles/index.css';
+import Button from './components/Button.vue';
 
 export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
-    DefaultTheme.enhanceApp(ctx)
-    useComponents(ctx.app)
-    ctx.app.component(Button.name, Button)
-  }
-}
+    DefaultTheme.enhanceApp(ctx);
+    useComponents(ctx.app);
+    ctx.app.component(Button.name, Button);
+  },
+};
