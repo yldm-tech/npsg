1c6e373486d6bcb763eb0fe2c8eb023c8bd007ae
Evan <suzukaze.hazuki2020@gmail.com>
2023-01-26 18:32:31 +0900

    fix(fix docs)

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -2,13 +2,11 @@ import DefaultTheme from 'vitepress/theme';
 import 'vitepress-theme-demoblock/dist/theme/styles/index.css';
 import { useComponents } from './useComponents';
 import './styles/index.css';
-import Button from './../components/Button.vue';
 
 export default {
   ...DefaultTheme,
   enhanceApp(ctx) {
     DefaultTheme.enhanceApp(ctx);
     useComponents(ctx.app);
-    ctx.app.component(Button.name, Button);
   },
 };
