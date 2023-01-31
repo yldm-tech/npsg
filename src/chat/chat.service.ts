0e80168596a6fdffc24831026706306fa88fe432
Evan <suzukaze.hazuki2020@gmail.com>
2023-02-01 00:02:50 +0900

    docs(chat): chat module docs

--- src/chat/chat.service.ts
+++ src/chat/chat.service.ts
@@ -6,12 +6,21 @@ import { ChatEntity } from './entities/chat.entity';
 @Injectable()
 export class ChatService {
   constructor(private readonly prisma: PrismaService) {}
+  /**
+   * 创建消息
+   * @param chat chat
+   * @returns
+   */
   async createMessage(chat: ChatEntity): Promise<Chat> {
     return await this.prisma.chat.create({
       data: chat,
     });
   }
 
+  /**
+   * 获取消息
+   * @returns 消息列表
+   */
   async getMessages(): Promise<Chat[]> {
     return await this.prisma.chat.findMany();
   }
